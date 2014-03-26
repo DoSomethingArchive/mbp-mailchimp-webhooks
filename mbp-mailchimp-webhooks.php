4ee73ed64cedf986591929863f89c82a8ff93f52
Jon Uy <jonxuy@gmail.com>
2014-03-26 12:53:48 -0400

    Nested queue option array. Fixes #5

--- mbp-mailchimp-webhooks.php
+++ mbp-mailchimp-webhooks.php
@@ -55,11 +55,14 @@ if ($_POST['type'] == 'unsubscribe') {
 
     // Queue options
     'queue' => array(
-      'name' => getenv('MB_MAILCHIMP_UNSUBSCRIBE_QUEUE'),
-      'passive' => getenv('MB_MAILCHIMP_UNSUBSCRIBE_QUEUE_PASSIVE'),
-      'durable' => getenv('MB_MAILCHIMP_UNSUBSCRIBE_QUEUE_DURABLE'),
-      'exclusive' => getenv('MB_MAILCHIMP_UNSUBSCRIBE_QUEUE_EXCLUSIVE'),
-      'auto_delete' => getenv('MB_MAILCHIMP_UNSUBSCRIBE_QUEUE_AUTO_DELETE'),
+      'mailchimp-webhook' => array(
+        'name' => getenv('MB_MAILCHIMP_UNSUBSCRIBE_QUEUE'),
+        'passive' => getenv('MB_MAILCHIMP_UNSUBSCRIBE_QUEUE_PASSIVE'),
+        'durable' => getenv('MB_MAILCHIMP_UNSUBSCRIBE_QUEUE_DURABLE'),
+        'exclusive' => getenv('MB_MAILCHIMP_UNSUBSCRIBE_QUEUE_EXCLUSIVE'),
+        'auto_delete' => getenv('MB_MAILCHIMP_UNSUBSCRIBE_QUEUE_AUTO_DELETE'),
+        'bindingKey' => getenv('MB_MAILCHIMP_UNSUBSCRIBE_ROUTING_KEY'),
+      ),
     ),
   );
 
